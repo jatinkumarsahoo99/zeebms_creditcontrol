69a0253e014b95fe67b99b66f94a546e16c31291
Mohd Maroof <vc.mohd.maroof@zee.com>
2024-01-04 10:58:06 +0530

    asrun import form created

---
+++ lib/app/modules/AsrunImportSecondaryEvents/controllers/asrun_import_secondary_events_controller.dart
@@ -0,0 +1,23 @@
+import 'package:get/get.dart';
+
+class AsrunImportSecondaryEventsController extends GetxController {
+  //TODO: Implement AsrunImportSecondaryEventsController
+
+  final count = 0.obs;
+  @override
+  void onInit() {
+    super.onInit();
+  }
+
+  @override
+  void onReady() {
+    super.onReady();
+  }
+
+  @override
+  void onClose() {
+    super.onClose();
+  }
+
+  void increment() => count.value++;
+}
--- lib/app/routes/app_pages.dart
+++ lib/app/routes/app_pages.dart
@@ -1,7 +1,10 @@
 import 'package:get/get.dart';
 
+import '../modules/AsrunImportSecondaryEvents/bindings/asrun_import_secondary_events_binding.dart';
+import '../modules/AsrunImportSecondaryEvents/views/asrun_import_secondary_events_view.dart';
 import '../modules/home/bindings/home_binding.dart';
 import '../modules/home/views/home_view.dart';
+import '../providers/AuthGuard1.dart';
 
 part 'app_routes.dart';
 
@@ -15,6 +18,13 @@ class AppPages {
       name: _Paths.HOME,
       page: () => HomeView(),
       binding: HomeBinding(),
+    ),
+    GetPage(
+      name: _Paths.ASRUN_IMPORT_SECONDARY_EVENTS,
+      page: () => AuthGuard(
+        childName: _Paths.ASRUN_IMPORT_SECONDARY_EVENTS,
+      ),
+      // binding: AsrunImportSecondaryEventsBinding(),
     ),
   ];
 }
