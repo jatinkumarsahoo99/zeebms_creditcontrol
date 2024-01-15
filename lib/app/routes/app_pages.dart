f1abe12abfbcc4fbf669c6bdc30c1fba2049ed6c
jatinkumarsahoo <jatnkumarsahoo99@gmail.com>
2024-01-15 11:52:02 +0530

    UI Design added

--- lib/app/routes/app_pages.dart
+++ lib/app/routes/app_pages.dart
@@ -94,7 +94,7 @@ class AppPages {
 
   static const INITIAL = kReleaseMode
       ? Routes.HOME
-      : (Routes.ASRUN_SPECIFICATION_AD_REVENUE +
+      : (Routes.CLIENT_DEALS +
           "?personalNo=R9vVPL7er1Os%2FusemWG%2FIw%3D%3D&loginCode=0iGe3vK5h2KGjfSKZTpmsQ%3D%3D&formName=0JUkJLseCJROuXQJxWn0tvQx2tD7hbegZhslvSOuxHotg9L%2FT284%2FQNIMZExXr81");
 
   static final routes = [
