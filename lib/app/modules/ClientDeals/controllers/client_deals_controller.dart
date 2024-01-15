2f2cc8e279aa7ea1e359d9342b9a20c60e82def0
jatinkumarsahoo <jatnkumarsahoo99@gmail.com>
2024-01-15 18:50:48 +0530

    client deals api integration

--- lib/app/modules/ClientDeals/controllers/client_deals_controller.dart
+++ lib/app/modules/ClientDeals/controllers/client_deals_controller.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 import '../../../../widgets/LoadingDialog.dart';
+import '../../../controller/ConnectorControl.dart';
+import '../../../providers/ApiFactory.dart';
 import '../../../providers/Utils.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:get/get.dart';
@@ -20,6 +22,7 @@ class ClientDealsController extends GetxController {
   RxList<DropDownValue> clientList = RxList([]);
   RxList<DropDownValue> agencyList = RxList([]);
   RxList<DropDownValue> brandList = RxList([]);
+  RxList<DropDownValue> brandList2 = RxList([]);
   RxList<DropDownValue> accountList = RxList([]);
   RxList<DropDownValue> subTypeList = RxList([]);
   RxList<DropDownValue> spotTypeList = RxList([]);
@@ -33,10 +36,22 @@ class ClientDealsController extends GetxController {
   RxList<DropDownValue> channelList2 = RxList([]);
 
   Rxn<DropDownValue>? selectedLocation;
+  Rxn<DropDownValue>? selectedLocation2;
   Rxn<DropDownValue>? selectedChannel;
+  Rxn<DropDownValue>? selectedChannel2;
   Rxn<DropDownValue>? selectedClient;
+  Rxn<DropDownValue>? selectedClient2;
   Rxn<DropDownValue>? selectPayMode;
   Rxn<DropDownValue>? selectCurrency;
+  Rxn<DropDownValue>? selectBrand;
+  Rxn<DropDownValue>? selectBrand2;
+  Rxn<DropDownValue>? selectAgency;
+  Rxn<DropDownValue>? selectDealType;
+  Rxn<DropDownValue>? selectAccount;
+  Rxn<DropDownValue>? selectSubType;
+  Rxn<DropDownValue>? selectSpotType;
+  Rxn<DropDownValue>? selectProgram;
+  Rxn<DropDownValue>? selectAddInfo;
 
   TextEditingController dealNoController = TextEditingController();
   TextEditingController dateController = TextEditingController();
@@ -45,6 +60,127 @@ class ClientDealsController extends GetxController {
   TextEditingController maxSpeedController = TextEditingController();
   TextEditingController amountController = TextEditingController();
   TextEditingController secondsController = TextEditingController();
+  TextEditingController fromDateController = TextEditingController();
+  TextEditingController toDateController = TextEditingController();
+  TextEditingController bkDurationController = TextEditingController();
+  TextEditingController bkAmountController = TextEditingController();
+  TextEditingController textFormFieldController = TextEditingController();
+  TextEditingController startTimeController = TextEditingController();
+  TextEditingController endTimeController = TextEditingController();
+  TextEditingController secondsController2 = TextEditingController();
+  TextEditingController ratePerTenSecondsController = TextEditingController();
+  TextEditingController amountController2 = TextEditingController();
+  TextEditingController valueRateController = TextEditingController();
+
+  Rx<bool> effectiveRate = Rx<bool>(false);
+  Rx<bool> type = Rx<bool>(false);
+  Rx<bool> weekDay = Rx<bool>(false);
+  Rx<bool> weekEnd = Rx<bool>(false);
+  Rx<bool> mon = Rx<bool>(false);
+  Rx<bool> tue = Rx<bool>(false);
+  Rx<bool> wed = Rx<bool>(false);
+  Rx<bool> thu = Rx<bool>(false);
+  Rx<bool> fri = Rx<bool>(false);
+  Rx<bool> sat = Rx<bool>(false);
+  Rx<bool> sun = Rx<bool>(false);
+
+  getAllLoadData(){
+    LoadingDialog.call();
+    try{
+      Get.find<ConnectorControl>().GETMETHODCALL(
+          api: ApiFactory.Client_Deal_GET_LOAD,
+          // "https://jsonkeeper.com/b/D537"
+          fun: (map) {
+            closeDialogIfOpen();
+            if(map is Map && map['onload'] != null){
+              if(map['onload']['location'] != null &&
+                  map['onload']['location'].length >0 ){
+                locationList.clear();
+                RxList<DropDownValue> dataList = RxList([]);
+                map['onload']['location'].forEach((e){
+                  dataList.add(DropDownValue.fromJsonDynamic(
+                      e, "locationCode", "locationName"));
+                });
+                locationList.addAll(dataList);
+                locationList.refresh();
+              }
+
+              if(map['onload']['dealTypeMaster'] != null &&
+                  map['onload']['dealTypeMaster'].length >0 ){
+                dealType.clear();
+                RxList<DropDownValue> dataList = RxList([]);
+                map['onload']['dealTypeMaster'].forEach((e){
+                  dataList.add(DropDownValue.fromJsonDynamic(
+                      e, "dealtypecode", "dealtypename"));
+                });
+                dealType.addAll(dataList);
+                dealType.refresh();
+              }
+
+              if(map['onload']['currencyTypeMaster'] != null &&
+                  map['onload']['currencyTypeMaster'].length >0 ){
+                currency.clear();
+                RxList<DropDownValue> dataList = RxList([]);
+                map['onload']['currencyTypeMaster'].forEach((e){
+                  dataList.add(DropDownValue.fromJsonDynamic(
+                      e, "currencyTypeCode", "currencyTypeName"));
+                });
+                currency.addAll(dataList);
+                currency.refresh();
+              }
+
+              if(map['onload']['searchLocation'] != null &&
+                  map['onload']['searchLocation'].length >0 ){
+                locationList2.clear();
+                RxList<DropDownValue> dataList = RxList([]);
+                map['onload']['searchLocation'].forEach((e){
+                  dataList.add(DropDownValue.fromJsonDynamic(
+                      e, "locationcode", "locationname"));
+                });
+                locationList2.addAll(dataList);
+                locationList2.refresh();
+              }
+
+              if(map['onload']['searchChannel'] != null &&
+                  map['onload']['searchChannel'].length >0 ){
+                channelList2.clear();
+                RxList<DropDownValue> dataList = RxList([]);
+                map['onload']['searchChannel'].forEach((e){
+                  dataList.add(DropDownValue.fromJsonDynamic(
+                      e, "channelcode", "channelname"));
+                });
+                channelList2.addAll(dataList);
+                channelList2.refresh();
+              }
+
+              if(map['onload']['sponsorTypeMaster'] != null &&
+                  map['onload']['sponsorTypeMaster'].length >0 ){
+                spotTypeList.clear();
+                RxList<DropDownValue> dataList = RxList([]);
+                map['onload']['sponsorTypeMaster'].forEach((e){
+                  dataList.add(DropDownValue.fromJsonDynamic(
+                      e, "sponsorTypeCode", "sponsorTypeName"));
+                });
+                spotTypeList.addAll(dataList);
+                spotTypeList.refresh();
+              }
+
+
+
+
+            }
+          });
+    }catch(e){
+      closeDialogIfOpen();
+    }
+
+  }
+
+  closeDialogIfOpen() {
+    if (Get.isDialogOpen ?? false) {
+      Get.back();
+    }
+  }
 
   @override
   void onInit() {
@@ -53,6 +189,7 @@ class ClientDealsController extends GetxController {
 
   @override
   void onReady() {
+    getAllLoadData();
     super.onReady();
   }
 
@@ -61,27 +198,6 @@ class ClientDealsController extends GetxController {
     super.onClose();
   }
 
-  // void calculateDuration() {
-  //   num secondStartTime =
-  //       Utils.oldBMSConvertToSecondsValue(value: startTime.text);
-  //   num secondEndTime =
-  //       Utils.oldBMSConvertToSecondsValue(value: endTime.text);
-
-  //   var sec;
-  //   if (startTime.text.length >= 11) {
-  //     if ((secondEom - secondSom) < 0) {
-  //       LoadingDialog.showErrorDialog("EOM should not be less than SOM.");
-  //     } else {
-  //       duration.value.text =
-  //           Utils.convertToTimeFromDouble(value: secondEom - secondSom);
-
-  //       sec = Utils.oldBMSConvertToSecondsValue(value: duration.value.text);
-  //     }
-  //   }
-
-  //   print(">>>>>>>>>" + duration.value.text);
-  //   print(">>>>>>>>>" + sec.toString());
-  // }
 
   void increment() => count.value++;
 }
