eeae3021c3bc608d5b6166a78b0bffd772827479
Mohd Maroof <vc.mohd.maroof@zee.com>
2024-01-16 18:16:23 +0530

    Export Billing FPC  api intregated

--- lib/app/modules/ExportBillingFPC/controllers/export_billing_f_p_c_controller.dart
+++ lib/app/modules/ExportBillingFPC/controllers/export_billing_f_p_c_controller.dart
@@ -1,9 +1,17 @@
+import 'package:bms_creditcontrol/app/controller/ConnectorControl.dart';
+import 'package:bms_creditcontrol/app/data/DropDownValue.dart';
+import 'package:bms_creditcontrol/app/providers/ApiFactory.dart';
+import 'package:bms_creditcontrol/widgets/CheckBox/multi_check_box.dart';
+import 'package:bms_creditcontrol/widgets/LoadingDialog.dart';
 import 'package:get/get.dart';
 
 class ExportBillingFPCController extends GetxController {
-  //TODO: Implement ExportBillingFPCController
+  var locations = RxList<MultiCheckBoxModel>();
+  var channels = RxList<MultiCheckBoxModel>();
 
-  final count = 0.obs;
+  List<DropDownValue> selectLocation = [];
+  List<DropDownValue> selectChannel = [];
+
   @override
   void onInit() {
     super.onInit();
@@ -19,5 +27,35 @@ class ExportBillingFPCController extends GetxController {
     super.onClose();
   }
 
-  void increment() => count.value++;
+  getLoadData() {
+    LoadingDialog.call();
+    Get.find<ConnectorControl>().GETMETHODCALL(
+      api: ApiFactory.AZURE_REDIRECT_UI,
+      fun: (Map map) {
+        Get.back();
+        locations.clear();
+        channels.clear();
+        var i = 0;
+        map["loadData"]["location"].forEach((e) {
+          locations.add(MultiCheckBoxModel(
+              DropDownValue(key: e["channelcode"], value: e["channelname"]),
+              false,
+              i));
+          i++;
+        });
+        i = 0;
+        map["loadData"]["channel"].forEach((e) {
+          channels.add(MultiCheckBoxModel(
+              DropDownValue(key: e["channelcode"], value: e["channelname"]),
+              false,
+              i));
+          i++;
+        });
+      },
+      // failed: (resp) {
+      //   Get.back();
+      //   LoadingDialog.showErrorDialog(resp.toString());
+      // }
+    );
+  }
 }
--- lib/app/providers/ApiFactory.dart
+++ lib/app/providers/ApiFactory.dart
@@ -679,17 +679,20 @@ class ApiFactory {
       "$BASE_URL/api/ModuleFormTransaction/GetModuleFormDetails?ModuleCode=$moduleCode";
   ///////////////////////// END MODULE FORM TRANSACTION API//////
 
-
 //////////////////////////// SECONDARY ASRUN MODIFICATION ////////////////////////////////
 
-  static String get SECONDARY_ASRUN_MODIFICATION_LOCATION => "$BASE_URL/api/SecondaryAsrunModi/GetLocations";
-  static String get SECONDARY_ASRUN_MODIFICATION_CHANNEL  => "$BASE_URL/api/SecondaryAsrunModi/GetChannels";
-  static String get SECONDARY_ASRUN_MODIFICATION_BIND_GRID  => "$BASE_URL/api/SecondaryAsrunModi/GetBindGrid";
-  static String get SECONDARY_ASRUN_MODIFICATION_GET_OK  => "$BASE_URL/api/SecondaryAsrunModi/GetOkClick";
-  static String get SECONDARY_ASRUN_MODIFICATION_GET_CLEAR_MISMATCH  => "$BASE_URL/api/SecondaryAsrunModi/GetClearmismatch";
-  static String get SECONDARY_ASRUN_MODIFICATION_SAVE  => "$BASE_URL/api/SecondaryAsrunModi/PostSave";
-
-
+  static String get SECONDARY_ASRUN_MODIFICATION_LOCATION =>
+      "$BASE_URL/api/SecondaryAsrunModi/GetLocations";
+  static String get SECONDARY_ASRUN_MODIFICATION_CHANNEL =>
+      "$BASE_URL/api/SecondaryAsrunModi/GetChannels";
+  static String get SECONDARY_ASRUN_MODIFICATION_BIND_GRID =>
+      "$BASE_URL/api/SecondaryAsrunModi/GetBindGrid";
+  static String get SECONDARY_ASRUN_MODIFICATION_GET_OK =>
+      "$BASE_URL/api/SecondaryAsrunModi/GetOkClick";
+  static String get SECONDARY_ASRUN_MODIFICATION_GET_CLEAR_MISMATCH =>
+      "$BASE_URL/api/SecondaryAsrunModi/GetClearmismatch";
+  static String get SECONDARY_ASRUN_MODIFICATION_SAVE =>
+      "$BASE_URL/api/SecondaryAsrunModi/PostSave";
 
   ///
   ///
@@ -704,15 +707,28 @@ class ApiFactory {
   static String get MOVIE_UPDATE_POST_UNDO_SPOT =>
       "$BASE_URL/api/MovieUpdate/PostUndoSpot";
 
- /// //////////////////////////////// Client Deals ////////////////////////////////////
+  /// //////////////////////////////// Client Deals ////////////////////////////////////
 
   static String get Client_Deal_GET_LOAD => "$BASE_URL/api/ClientDeals/onload";
-  static String get Client_Deal_GET_CHANNEL => "$BASE_URL/api/ClientDeals/GetChannel?locationCode=";
-  static String get Client_Deal_GET_CLIENTS => "$BASE_URL/api/ClientDeals/GetClients";
-  static String get Client_Deal_GET_CLIENTS_LEAVE => "$BASE_URL/api/ClientDeals/ClientLeave";
-  static String get Client_Deal_GET_CHANNEL_LEAVE => "$BASE_URL/api/ClientDeals/ChannelLeave";
-  static String get Client_Deal_GET_SUBTYPE => "$BASE_URL/api/ClientDeals/GetSubType?accountCode=";
-  static String get Client_Deal_RETRIVE_RECORD => "$BASE_URL/api/ClientDeals/RetrieveRecord";
+  static String get Client_Deal_GET_CHANNEL =>
+      "$BASE_URL/api/ClientDeals/GetChannel?locationCode=";
+  static String get Client_Deal_GET_CLIENTS =>
+      "$BASE_URL/api/ClientDeals/GetClients";
+  static String get Client_Deal_GET_CLIENTS_LEAVE =>
+      "$BASE_URL/api/ClientDeals/ClientLeave";
+  static String get Client_Deal_GET_CHANNEL_LEAVE =>
+      "$BASE_URL/api/ClientDeals/ChannelLeave";
+  static String get Client_Deal_GET_SUBTYPE =>
+      "$BASE_URL/api/ClientDeals/GetSubType?accountCode=";
+  static String get Client_Deal_RETRIVE_RECORD =>
+      "$BASE_URL/api/ClientDeals/RetrieveRecord";
 
+  ///
+  ///
+  ///
+  /// ////////////////////////// Start Export Billing FPC Model////////////////////////////////
+  // static String get MOVIE_UPDATE_GET_LOAD =>
+  //     "$BASE_URL/api/MovieUpdate/GetLoad";
 
+  /// ////////////////////////// End Export Billing FPC Model////////////////////////////////
 }
